77081028411416adf9be150a0de28b43d8825763
thgbonfim <thiagobonfim73@gmail.com>
2025-05-30 13:19:03 -0300

    primeiro commit com collection e script Newman

---
+++ run-tests.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Instala Newman
+npm install -g newman
+
+REM Garante que a pasta de saída existe
+mkdir newman
+
+REM Executa os testes com base nos arquivos que vieram do Git
+newman run Minha_API.postman_collection.json ^
+  -r cli,html ^
+  --reporter-html-export newman\newman-run-report.html
+
+exit 0
